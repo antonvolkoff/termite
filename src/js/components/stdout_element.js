8b29218def6acca2b105921791944637ec2ae0eb
Anton Volkov <choixer@gmail.com>
2015-11-23 17:43:16 +0100

    splite stdout by lines

--- src/js/components/stdout_element.js
+++ src/js/components/stdout_element.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 class StdoutElement extends React.Component {
   render() {
+    let lines = this.props.element.value.split("\n").map((line) => {
+      return <div>{line}</div>
+    });
+
     return (
       <div className="element element-stdout">
-        {this.props.element.value}
+        {lines}
       </div>
     )
   }
