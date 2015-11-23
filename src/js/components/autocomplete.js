7f43aac270fe885fa27456c1844e01d69c6fb802
Anton Volkov <choixer@gmail.com>
2015-11-23 20:34:37 +0100

    Implement autocomplete logic

--- src/js/components/autocomplete.js
+++ src/js/components/autocomplete.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 class Autocomplete extends React.Component {
   render() {
-    let elements = this.props.items.map((item) => {
-      return <div>{item.name}</div>
+    let elements = this.props.suggestions.map((item) => {
+      let klass = "item";
+      if (item.id == this.props.selected.id) {
+        klass += " selected";
+      }
+
+      return <div key={item.id} className={klass}>{item.name}</div>
     })
 
     return (
--- src/js/components/stdin_element.js
+++ src/js/components/stdin_element.js
@@ -6,17 +6,21 @@ import TerminalActions  from '../actions/terminal_actions'
 import Autocomplete     from './autocomplete'
 
 let candidates = [
-  { name: 'ls()'    },
-  { name: 'pwd()'   },
-  { name: 'cd()'    },
-  { name: 'exec()'  },
-  { name: 'git.status()' },
+  { id: 0, name: 'ls()'    },
+  { id: 1, name: 'pwd()'   },
+  { id: 2, name: 'cd()'    },
+  { id: 3, name: 'exec()'  },
+  { id: 4, name: 'git.status()' },
 ];
 
 class StdinElement extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: props.element.value };
+    this.state = {
+      value:              props.element.value,
+      suggestions:        [],
+      selectedSuggestion: null,
+    };
 
     this.handleChange   = this.handleChange.bind(this);
     this.handleKeyDown  = this.handleKeyDown.bind(this);
@@ -28,8 +32,8 @@ class StdinElement extends React.Component {
 
   render() {
     let autocomplete = null;
-    if (!this.props.element.readOnly && this.state.value.length > 0) {
-      autocomplete = <Autocomplete items={filter(candidates, this.state.value, {key: 'name'})} />
+    if (!this.props.element.readOnly && this.state.suggestions.length > 0) {
+      autocomplete = <Autocomplete suggestions={this.state.suggestions} selected={this.state.selectedSuggestion} />
     }
 
     return (
@@ -48,13 +52,48 @@ class StdinElement extends React.Component {
 
     let state = this.state;
     state.value = value;
+    if (state.value.length > 0) {
+      state.suggestions = filter(candidates, state.value, {key: 'name'});
+      state.selectedSuggestion = state.suggestions[0];
+    } else {
+      state.suggestions = [];
+      state.selectedSuggestion = null;
+    }
+
     this.setState(state);
   }
 
   handleKeyDown(event) {
-    if (event.keyCode == 13) {
+    if (event.keyCode == 13) { // ENTER
       event.preventDefault();
       TerminalActions.execute(this.state.value);
+    }
+
+    if (event.keyCode == 40) { // DOWN
+      event.preventDefault();
+      let idx = this.state.suggestions.indexOf(this.state.selectedSuggestion);
+      if (idx < this.state.suggestions.length - 1) {
+        let state = this.state;
+        state.selectedSuggestion = this.state.suggestions[idx + 1];
+        this.setState(state);
+      }
+    }
+
+    if (event.keyCode == 38) { // UP
+      event.preventDefault();
+      let idx = this.state.suggestions.indexOf(this.state.selectedSuggestion);
+      if (idx != 0) {
+        let state = this.state;
+        state.selectedSuggestion = this.state.suggestions[idx - 1];
+        this.setState(state);
+      }
+    }
+
+    if (event.keyCode == 9) { // TAB
+      event.preventDefault();
+      let state = this.state;
+      state.value = state.selectedSuggestion.name;
+      this.setState(state);
     }
   }
 }
